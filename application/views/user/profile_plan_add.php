6d54cd7237c20cd9d54af1048f7c116bc191403c
sidmustdie <siddharthagupta94@gmail.com>
2021-05-17 10:22:51 +0530

    minor bug fixed

--- application/views/user/profile_plan_add.php
+++ application/views/user/profile_plan_add.php
@@ -83,6 +83,45 @@
             </div>
           </div>
           <!-- /.card -->
+
+          <div class="card card-info">
+            <div class="card-header">
+              <h3 class="card-title"><i class="fas fa-phone-alt mr-1"></i>VoIP Plan</h3>
+            </div>
+            <div class="card-body">
+              <div class="form-row">
+                <div class="form-group col-md">
+                  <label for="selectVoipNo">VoIP #</label>
+                  <select name="voip_id" class="custom-select" id="selectVoipNo">
+                    <option value="">Choose...</option>
+                      <?php foreach ($voip as $row) : ?>
+                          <option value="<?php echo $row->voip_id; ?>"><?php echo $row->voip_no.' - '.$row->vendor_name; ?></option>
+                      <?php endforeach; ?>
+                  </select>
+                </div>
+                <div class="form-group col-md">
+                  <label for="inputVoipRate">Plan Rate</label>
+                  <input type="number" class="form-control" name="voip_rate" id="inputVoipRate">
+                </div>
+              </div>
+              <div class="form-row">
+                <div class="form-group col-md">
+                  <label for="inputSetDep">VoIP Set Deposit</label>
+                  <input type="number" class="form-control" name="voip_charge" id="inputSetDep">
+                </div>
+                <div class="form-group col-md">
+                  <label for="inputVoipSetRef">VoIP Set Refund</label>
+                  <input type="number" class="form-control" name="voip_refund" id="inputVoipSetRef">
+                </div>
+              </div>
+            </div>
+            <!-- /.card-body -->
+            <div class="card-footer">
+              <button class="btn btn-primary float-right" name="submit" type="submit">Submit</button>
+            </div>
+          </div>
+          <!-- /.card -->
+
         <?php echo form_close(); ?>
       </div>
       <!-- /.col -->
