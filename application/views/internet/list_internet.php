7d4bf8013e1411c069a429e9df9adc49895f70e6
sidmustdie <siddharthagupta94@gmail.com>
2021-05-18 17:55:16 +0530

    Fixing

--- application/views/internet/list_internet.php
+++ application/views/internet/list_internet.php
@@ -7,7 +7,7 @@
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
           <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-          <li class="breadcrumb-item active">Internet Plan Management</li>
+          <li class="breadcrumb-item active">Internet Plan</li>
         </ol>
       </div><!-- /.col -->
     </div><!-- /.row -->
